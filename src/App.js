d2f306cb0483aa7dbd05113a24afbef452cb0085
davidcopano <davidcopano96@gmail.com>
2018-05-20 16:21:12 +0200

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Calendar } from "./components/Calendar/Calendar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Calendar/>
+  }
+}
+
+export default App;
